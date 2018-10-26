0287fdf2c267c959cec7ef43a49ce90dc150af4d
liug <liugang@zbj.com>
2018-10-26 15:54:24 +0800

    test jenkins trigger

---
+++ rabbitmq/src/main/java/Test.java
@@ -0,0 +1,10 @@
+/**
+ * @author lg
+ * @date 2018-10-26
+ * &Desc for jenkins
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("test trigger action by jenkins");
+    }
+}
